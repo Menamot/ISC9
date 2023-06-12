61e60c0e8b605b9201ef6d838d54ed0b5705e3c9
Menamot <menamot.chen@gmail.com>
2023-06-12 19:58:04 +0200

    实现了能量计算和PID算法

--- analogique.m
+++ analogique.m
@@ -1,73 +1,71 @@
 % Paramètres et grandeurs physiques
-M_MPM = 0.38e-3; % kg
-P_MPM = M_MPM * 9.81; % N
-course = 0.1e-3; % m
-mu_f = 0.4; % 无单位
-
-% vecteur de temps
-t = linspace(0, 0.01, 1000); % s
-
-% position x vecteur
-pos_x = linspace(0, course, 1000); % m
-
-% Calculer Fx_M, Fx_EM et Fz_EM
-ix = 1; % Supposons que le courant est de 1A
-Fx_EM = ix * (2.1252E+18 * pos_x.^6 - 9.7281E+07 * pos_x.^5 - 5.0226E+10 * pos_x.^4 + 1.3028E+00 * pos_x.^3 + 4.9317E+02 * pos_x.^2 - 1.0385E-07 * pos_x + 1.3115E-03);
-Fz_EM = ix * (-1.4456E+07 * pos_x.^3 + 1.2642E-05 * pos_x.^2 - 2.9536E-01 * pos_x + 9.3282E-14);
-
-% Calculer Ftotal_x (la composante de la force résultante sur l'axe des x)
-Ftotal_x = Fx_EM;
-
-% Calculer Ftotal_z (la composante de la force résultante sur l'axe z)
-Ftotal_z = P_MPM + Fz_EM;
-
-% Calculer le poids net
-Wnet = M_MPM * 9.81 - Ftotal_z;
-
-% Calculer le frottement
-Ff = Wnet * mu_f;
-
-% Calculer l'accélération
-a = (Ftotal_x - Ff) / M_MPM;
-
-% Calculer la vitesse à l'aide de la fonction intégrale
-v = cumtrapz(t, a);
-
-% Calculer le déplacement à l'aide de la fonction intégrale
-x = cumtrapz(t, v);
-index = find(x>course);
-x(index(1):end)=course;
-v(index(1):end)=0;
-a(index(1):end)=0;
-Ftotal_x(index(1):end)=0;
 
 
+best_Kp = 2.8;
+best_Ki = 0;
+best_Kd = 1.5;
+% pid(best_Kp,best_Ki,best_Kd)
+pid(0.8,0,3.4/8);
+st=1000;
+% for Kp=0.1:0.1:3
+%     for Ki=0:0.1:3
+%         for Kd=0:0.1:3
+%             settled_time=pid(Kp,Ki,Kd);
+%             if settled_time<st
+%                 st=settled_time;
+%                 best_Kp=Kp;
+%                 best_Ki=Ki;
+%                 best_Kd=Kd;
+%             end
+%         end 
+%     end 
+% end
 
-% Tracer la force, l'accélération, la vitesse et le déplacement dans le temps
-figure;
-subplot(4, 1, 1);
-plot(t, Ftotal_x);
-title('Force vs. Time');
-ylabel('Force (N)');
-xlabel('Time (s)');
+function st=pid(Kp,Ki,Kd)
+    M_MPM = 0.38e-3; 
+    P_MPM = M_MPM * 9.81; 
+    course = 0.1e-3; 
+    mu_f = 0.4; 
+    E=0;
+    R=1;
+    simulation_time = 20;
+    dt = 0.01;
+    C_R=0.75;
+    object_pos = 0;
+    object_acc = 0;
+    object_vite = 0;
+    integral_term=0;
+    
+    x = [];
+    v=[];
+    a=[];
+    fx=[];
+    e=[];
+    for t = 0:dt:simulation_time
+        error = course - object_pos;
+        control_ix = Kp * error + Ki * integral_term - Kd * object_vite;
+        e=[e error];
+        Fx_EM = control_ix *(2.1252E+18 * object_pos.^6 - 9.7281E+07 * object_pos.^5 - 5.0226E+10 * object_pos.^4 + 1.3028E+00 * object_pos.^3 + 4.9317E+02 * object_pos.^2 - 1.0385E-07 * object_pos + 1.3115E-03);
+        Fz_EM = control_ix *(-1.4456E+07 * object_pos.^3 + 1.2642E-05 * object_pos.^2 - 2.9536E-01 * object_pos + 9.3282E-14);
+        
+        Ftotal_x = Fx_EM;
+        Ftotal_z = P_MPM + Fz_EM;
+        Wnet = M_MPM * 9.81 - Ftotal_z;
+        Ff = Wnet * mu_f;
+        fx=[fx Ftotal_x];
+        object_acc = (Ftotal_x - Ff) / M_MPM;
+        object_vite = object_vite + object_acc * dt;
+        object_pos = object_pos + object_vite * dt;
+        
+        integral_term = integral_term + error * dt;
+        E = E+control_ix.^2*R*dt;
+        x = [x object_pos];
+        a = [a object_acc];
+        v = [v object_vite];
+    end
+    index = find(abs(e)>0.01*course);
+    st = index(end) * dt;
+    plot(x);
+    fprintf('Total energie:%s\n',E);
+end
 
-subplot(4, 1, 2);
-plot(t, a);
-title('Acceleration vs. Time');
-ylabel('Acceleration (m/s^2)');
-xlabel('Time (s)');
-
-subplot(4, 1, 3);
-plot(t, v);
-title('Velocity vs. Time');
-ylabel('Velocity (m/s)');
-xlabel('Time (s)');
-
-subplot(4, 1, 4);
-plot(t, x);
-title('Displacement vs. Time');
-ylabel('Displacement (m)');
-xlabel('Time (s)');
-
-% figure;
-% plot(pos_x, Fx_EM);
